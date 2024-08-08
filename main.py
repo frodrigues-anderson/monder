d9d7e1de3514fc10d99031475fd6e9c59b335350
frodrigues-anderson <andfr2005@gmail.com>
2024-08-08 03:14:05 -0300

    Compilado VS code

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ from paginas.moneta import pagina_moneta
 from paginas.backtestes import pagina_backtestes
 from simbolos import simbolos
 import pandas as pd
-import dash
-import dash_bootstrap_components as dbc
-from dash import html, dcc, Input, Output
-from dash_bootstrap_templates import ThemeSwitchAIO
-import plotly.graph_objects as go
-import plotly.express as px
-import calendar
-import locale
+
 
 
 # cria um dicionário com as páginas disponíveis com os nomes das páginas como chave 
@@ -35,12 +28,6 @@ def main():
     # Chama/invoca a página desejada
     paginas[modelo_selecionado](simbolos, paises, intervalos)
      
-
-# Criando App __________________________________________________________________________________________
-
-app = dash.Dash(__name__)
-
-server = app.server
 
 if __name__ == "__main__":
     main()
